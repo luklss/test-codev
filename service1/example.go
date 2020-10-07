54b89ccf723089d958b562a75ea6bcc2e2ba7b60
luklss <lucasklafkebeck@gmail.com>
2020-10-07 14:06:23 +0200

    adding a func

--- service1/example.go
+++ service1/example.go
@@ -7,3 +7,13 @@ func sum(a, b int) int {
 func multiply(a, b int) int {
 	return a * b
 }
+
+func pow(base, power int) int {
+	result := 1
+	for i := 0; i < power; i++ {
+		result = result * base
+	}
+
+	return result
+
+}
